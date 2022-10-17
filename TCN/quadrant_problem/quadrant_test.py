5228a5917acc9af242b91818bf093bd5919a0f34
Aaron Potvien <aaron.potvien@gmail.com>
2022-10-17 16:14:05 +0000

    style: black

--- TCN/quadrant_problem/quadrant_test.py
+++ TCN/quadrant_problem/quadrant_test.py
@@ -3,21 +3,22 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 import sys
+
 sys.path.append("../../")
 from TCN.adding_problem.model import TCN
 
 # Removed argparse so it's a easier to step through in an interactive environment
 batch_size = 2
-cuda = True # Assumes GPU is available
+cuda = True  # Assumes GPU is available
 dropout = 0.0
 clip = 1
 epochs = 10
 ksize = 2
 levels = 8
-seq_len = 5 # Number of characters to generate in the string that is to be predicted
+seq_len = 5  # Number of characters to generate in the string that is to be predicted
 log_interval = 100
 lr = 4e-3
-optimizer_type = 'Adam'
+optimizer_type = "Adam"
 nhid = 30
 seed = 1111
 
@@ -30,11 +31,13 @@ input_channels = 1
 n_classes = seq_len
 num_train_samples = 800
 num_test_samples = 200
-LOW, HIGH = -20, 20 # Bounds for uniform ints
+LOW, HIGH = -20, 20  # Bounds for uniform ints
+
 
 def random_int_generator():
     while True:
         yield np.random.randint(LOW, HIGH)
+
 
 rng = random_int_generator()
 
@@ -42,28 +45,31 @@ cat_to_num = {
     "A": 1.0,
     "B": 2.0,
     "C": 3.0,
-} # First pass mapping to floats like the adding problem
+}  # First pass mapping to floats like the adding problem
+
 
 def get_random_xy():
     x0, y0 = next(rng), next(rng)
-    if x0 < 0 and y0 < 0: # Quadrant III
+    if x0 < 0 and y0 < 0:  # Quadrant III
         c0 = "B"
-    elif x0 < 0 and y0 > 0: # Quadrant II
+    elif x0 < 0 and y0 > 0:  # Quadrant II
         c0 = "A"
-    elif x0 > 0 and y0 > 0: # Quadrant I
+    elif x0 > 0 and y0 > 0:  # Quadrant I
         c0 = "C"
-    else: # Quadrant IV
-        c0 = np.random.choice(["A","B","C"])
+    else:  # Quadrant IV
+        c0 = np.random.choice(["A", "B", "C"])
 
     c0 = torch.tensor([cat_to_num[c0]])
     xy = torch.tensor([float(x0), float(y0)])
     return (xy, c0)
+
 
 def get_random_sequence(seq_len):
     points = [get_random_xy() for _ in range(seq_len)]
     x_data = torch.hstack([x[0] for x in points])
     y_data = torch.hstack([x[1] for x in points])
     return x_data, y_data
+
 
 # Generate train/test data
 train_list = [get_random_sequence(seq_len) for _ in range(num_train_samples)]
@@ -78,9 +84,11 @@ X_train = X_train[:, None, :]
 X_test = X_test[:, None, :]
 
 # Note: We use a very simple setting here (assuming all levels have the same # of channels.
-channel_sizes = [nhid]*levels
+channel_sizes = [nhid] * levels
 kernel_size = ksize
-model = TCN(input_channels, n_classes, channel_sizes, kernel_size=kernel_size, dropout=dropout)
+model = TCN(
+    input_channels, n_classes, channel_sizes, kernel_size=kernel_size, dropout=dropout
+)
 
 if cuda:
     model.cuda()
@@ -101,7 +109,7 @@ def train(epoch):
         if i + batch_size > X_train.size(0):
             x, y = X_train[i:], y_train[i:]
         else:
-            x, y = X_train[i:(i+batch_size)], y_train[i:(i+batch_size)]
+            x, y = X_train[i : (i + batch_size)], y_train[i : (i + batch_size)]
         optimizer.zero_grad()
         output = model(x)
         loss = F.mse_loss(output, y)
@@ -114,9 +122,17 @@ def train(epoch):
 
         if batch_idx % log_interval == 0:
             cur_loss = total_loss / log_interval
-            processed = min(i+batch_size, X_train.size(0))
-            print('Train Epoch: {:2d} [{:6d}/{:6d} ({:.0f}%)]\tLearning rate: {:.4f}\tLoss: {:.6f}'.format(
-                epoch, processed, X_train.size(0), 100.*processed/X_train.size(0), lr, cur_loss))
+            processed = min(i + batch_size, X_train.size(0))
+            print(
+                "Train Epoch: {:2d} [{:6d}/{:6d} ({:.0f}%)]\tLearning rate: {:.4f}\tLoss: {:.6f}".format(
+                    epoch,
+                    processed,
+                    X_train.size(0),
+                    100.0 * processed / X_train.size(0),
+                    lr,
+                    cur_loss,
+                )
+            )
             total_loss = 0
 
 
@@ -125,13 +141,10 @@ def evaluate():
     with torch.no_grad():
         output = model(X_test)
         test_loss = F.mse_loss(output, y_test)
-        print('\nTest set: Average loss: {:.6f}\n'.format(test_loss.item()))
+        print("\nTest set: Average loss: {:.6f}\n".format(test_loss.item()))
         return test_loss.item()
 
 
-for ep in range(1, epochs+1):
+for ep in range(1, epochs + 1):
     train(ep)
     tloss = evaluate()
-
-
-
