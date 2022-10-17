cec41c4e894fcfb4035acffe3b57c2bf4787ff99
Aaron Potvien <aaron.potvien@gmail.com>
2022-10-17 16:12:54 +0000

    implement data generator for character sequences

--- TCN/quadrant_problem/quadrant_test.py
+++ TCN/quadrant_problem/quadrant_test.py
@@ -14,7 +14,7 @@ clip = 1
 epochs = 10
 ksize = 2
 levels = 8
-seq_len = 400
+seq_len = 5 # Number of characters to generate in the string that is to be predicted
 log_interval = 100
 lr = 4e-3
 optimizer_type = 'Adam'
@@ -26,15 +26,56 @@ if torch.cuda.is_available():
     if not cuda:
         print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
-input_channels = 2
-n_classes = 3
+input_channels = 1
+n_classes = seq_len
+num_train_samples = 800
+num_test_samples = 200
 LOW, HIGH = -20, 20 # Bounds for uniform ints
 
 def random_int_generator():
-    yield np.random.randint(LOW, HIGH)
+    while True:
+        yield np.random.randint(LOW, HIGH)
 
-# TODO: Generate datasets
+rng = random_int_generator()
 
+cat_to_num = {
+    "A": 1.0,
+    "B": 2.0,
+    "C": 3.0,
+} # First pass mapping to floats like the adding problem
+
+def get_random_xy():
+    x0, y0 = next(rng), next(rng)
+    if x0 < 0 and y0 < 0: # Quadrant III
+        c0 = "B"
+    elif x0 < 0 and y0 > 0: # Quadrant II
+        c0 = "A"
+    elif x0 > 0 and y0 > 0: # Quadrant I
+        c0 = "C"
+    else: # Quadrant IV
+        c0 = np.random.choice(["A","B","C"])
+
+    c0 = torch.tensor([cat_to_num[c0]])
+    xy = torch.tensor([float(x0), float(y0)])
+    return (xy, c0)
+
+def get_random_sequence(seq_len):
+    points = [get_random_xy() for _ in range(seq_len)]
+    x_data = torch.hstack([x[0] for x in points])
+    y_data = torch.hstack([x[1] for x in points])
+    return x_data, y_data
+
+# Generate train/test data
+train_list = [get_random_sequence(seq_len) for _ in range(num_train_samples)]
+X_train = torch.stack([x[0] for x in train_list])
+y_train = torch.stack([x[1] for x in train_list])
+
+test_list = [get_random_sequence(seq_len) for _ in range(num_test_samples)]
+X_test = torch.stack([x[0] for x in test_list])
+y_test = torch.stack([x[1] for x in test_list])
+
+X_train = X_train[:, None, :]
+X_test = X_test[:, None, :]
 
 # Note: We use a very simple setting here (assuming all levels have the same # of channels.
 channel_sizes = [nhid]*levels
@@ -44,9 +85,9 @@ model = TCN(input_channels, n_classes, channel_sizes, kernel_size=kernel_size, d
 if cuda:
     model.cuda()
     X_train = X_train.cuda()
-    Y_train = Y_train.cuda()
+    y_train = y_train.cuda()
     X_test = X_test.cuda()
-    Y_test = Y_test.cuda()
+    y_test = y_test.cuda()
 
 optimizer = getattr(optim, optimizer_type)(model.parameters(), lr=lr)
 
@@ -58,9 +99,9 @@ def train(epoch):
     total_loss = 0
     for i in range(0, X_train.size(0), batch_size):
         if i + batch_size > X_train.size(0):
-            x, y = X_train[i:], Y_train[i:]
+            x, y = X_train[i:], y_train[i:]
         else:
-            x, y = X_train[i:(i+batch_size)], Y_train[i:(i+batch_size)]
+            x, y = X_train[i:(i+batch_size)], y_train[i:(i+batch_size)]
         optimizer.zero_grad()
         output = model(x)
         loss = F.mse_loss(output, y)
@@ -83,7 +124,7 @@ def evaluate():
     model.eval()
     with torch.no_grad():
         output = model(X_test)
-        test_loss = F.mse_loss(output, Y_test)
+        test_loss = F.mse_loss(output, y_test)
         print('\nTest set: Average loss: {:.6f}\n'.format(test_loss.item()))
         return test_loss.item()
 
