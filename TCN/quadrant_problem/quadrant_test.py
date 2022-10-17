7dd5795145753ff5b72d6d962e3fcf71350b4379
Aaron Potvien <aaron.potvien@gmail.com>
2022-10-17 15:06:13 +0000

    remove args remenants

--- TCN/quadrant_problem/quadrant_test.py
+++ TCN/quadrant_problem/quadrant_test.py
@@ -1,10 +1,10 @@
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
+import numpy as np
 import sys
 sys.path.append("../../")
 from TCN.adding_problem.model import TCN
-from TCN.adding_problem.utils import data_generator
 
 # Removed argparse so it's a easier to step through in an interactive environment
 batch_size = 2
@@ -27,29 +27,28 @@ if torch.cuda.is_available():
         print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
 input_channels = 2
-n_classes = 1
+n_classes = 3
+LOW, HIGH = -20, 20 # Bounds for uniform ints
 
-print(args)
-print("Producing data...")
-X_train, Y_train = data_generator(50000, seq_length)
-X_test, Y_test = data_generator(1000, seq_length)
+def random_int_generator():
+    yield np.random.randint(LOW, HIGH)
+
+# TODO: Generate datasets
 
 
 # Note: We use a very simple setting here (assuming all levels have the same # of channels.
-channel_sizes = [args.nhid]*args.levels
-kernel_size = args.ksize
-dropout = args.dropout
+channel_sizes = [nhid]*levels
+kernel_size = ksize
 model = TCN(input_channels, n_classes, channel_sizes, kernel_size=kernel_size, dropout=dropout)
 
-if args.cuda:
+if cuda:
     model.cuda()
     X_train = X_train.cuda()
     Y_train = Y_train.cuda()
     X_test = X_test.cuda()
     Y_test = Y_test.cuda()
 
-lr = args.lr
-optimizer = getattr(optim, args.optim)(model.parameters(), lr=lr)
+optimizer = getattr(optim, optimizer_type)(model.parameters(), lr=lr)
 
 
 def train(epoch):
@@ -66,14 +65,14 @@ def train(epoch):
         output = model(x)
         loss = F.mse_loss(output, y)
         loss.backward()
-        if args.clip > 0:
-            torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+        if clip > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
         optimizer.step()
         batch_idx += 1
         total_loss += loss.item()
 
-        if batch_idx % args.log_interval == 0:
-            cur_loss = total_loss / args.log_interval
+        if batch_idx % log_interval == 0:
+            cur_loss = total_loss / log_interval
             processed = min(i+batch_size, X_train.size(0))
             print('Train Epoch: {:2d} [{:6d}/{:6d} ({:.0f}%)]\tLearning rate: {:.4f}\tLoss: {:.6f}'.format(
                 epoch, processed, X_train.size(0), 100.*processed/X_train.size(0), lr, cur_loss))
